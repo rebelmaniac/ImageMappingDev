15443bb90257ed76563984e5f964915628197f94
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2023-03-01 09:13:08 -0500

    position absolute is set

--- src/styles.ts
+++ src/styles.ts
@@ -28,7 +28,7 @@ interface StylesProps {
 
 const styles = (props?: Partial<ImageMapperProps>): StylesProps => ({
   container: {
-    position: 'relative',
+    position: 'absolute',
     width: 'unset'
   },
   canvas: {
