7db2b09e528294cef9e60d2d9cf4ae2f67e13bf2
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2023-03-21 07:58:40 -0400

    Update styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -28,8 +28,7 @@ interface StylesProps {
 
 const styles = (props?: Partial<ImageMapperProps>): StylesProps => ({
   container: {
-    position: 'absolute',
-    margin: 'auto'
+    position: 'relative',
   },
   canvas: {
     ...absPos,
