04c485d633dcbbc8a686b69eea82e42d4f1f21c0
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2023-03-23 03:04:18 -0400

    Revert "Update styles.ts"
    This reverts commit 7db2b09e528294cef9e60d2d9cf4ae2f67e13bf2.

--- src/styles.ts
+++ src/styles.ts
@@ -28,7 +28,8 @@ interface StylesProps {
 
 const styles = (props?: Partial<ImageMapperProps>): StylesProps => ({
   container: {
-    position: 'relative',
+    position: 'absolute',
+    margin: 'auto'
   },
   canvas: {
     ...absPos,
